23a8fcc2249fab0739d25f0612aae928f45d748c
Maria Chira <geaguleamaria@yahoo.com>
2022-03-05 23:19:42 +0200

    .

--- budget-app/src/components/accordionIncome/accordionIncome.jsx
+++ budget-app/src/components/accordionIncome/accordionIncome.jsx
@@ -33,11 +33,11 @@ const AccordionIncome = () => {
       <Accordion>
         <AccordionCard cardTitle="Active Income" eventKeyNo="1">
           <CardIncome handleOnChange={contentChange} incomeType="active" />
-          <DescriptionCard text="ex:salaries, bonuses, commissions"></DescriptionCard>
+          <DescriptionCard text="ex: salaries, bonuses, commissions"></DescriptionCard>
         </AccordionCard>
         <AccordionCard cardTitle="Passive Income" eventKeyNo="2">
           <CardIncome handleOnChange={contentChange} incomeType="passive" />
-          <DescriptionCard text="ex:dividendes, rents, interest, copyright. (you should put the net income, after taxes) "></DescriptionCard>
+          <DescriptionCard text="ex: dividendes, rents, interest, copyright. (you should put the net income, after taxes) "></DescriptionCard>
         </AccordionCard>
       </Accordion>
       <IncomeTotal incomeTotal={incomeTotal} />
